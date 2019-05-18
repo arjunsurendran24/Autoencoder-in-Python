c1058cd782e8d24e5637bff76619bb5e3ae500e0
Arjun Surendran <arjun.surendran@yahoo.in>
2019-05-18 12:30:32 -0700

    Add files via upload

---
+++ autoencoder_pytorch.py
@@ -0,0 +1,79 @@
+import torch
+
+import torchvision as tv
+
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+
+import torch.nn.functional as F
+
+from torch.autograd import Variable
+
+from torchvision.utils import save_image
+
+# Loading and Transforming data
+
+transform = transforms.Compose([transforms.ToTensor()])
+
+trainTransform  = tv.transforms.Compose([tv.transforms.ToTensor()])
+
+trainset = tv.datasets.MNIST(root='./data',  train=True,download=True, transform=transform)
+
+dataloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False, num_workers=4)
+
+testset = tv.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+# Writing our model
+
+class Autoencoder(nn.Module):
+
+    def __init__(self):
+        super(Autoencoder,self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.Conv2d(6,16,kernel_size=5),
+            nn.ReLU(True))
+
+        self.decoder = nn.Sequential(             
+            nn.ConvTranspose2d(16,6,kernel_size=5),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(6,1,kernel_size=5),
+            nn.ReLU(True),
+            nn.Sigmoid())
+
+    def forward(self,x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+#defining some params
+
+num_epochs = 5
+
+batch_size = 128
+
+model = Autoencoder().cpu()
+
+distance = nn.MSELoss()
+
+optimizer = torch.optim.Adam(model.parameters(),weight_decay=1e-5)
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data
+        img = Variable(img).cpu()
+        # ===================forward=====================
+        output = model(img)
+        loss = distance(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}'.format(epoch+1, num_epochs, loss.item()))
